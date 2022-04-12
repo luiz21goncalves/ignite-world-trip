d08b8ee9a50becc4d05d28a44ef9ef28153adc15
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-04-12 20:29:08 -0300

    refactor(Header): add responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import { Flex, Image } from '@chakra-ui/react';
 
 export function Header() {
   return (
-    <Flex w="full" align="center" justify="center" py={4}>
-      <Image src="assets/logo.svg" w={20} h={5} alt="logo" />
+    <Flex w="full" align="center" justify="center" py={[4, 7]}>
+      <Image src="assets/logo.svg" w={[20, 48]} h={[5, 12]} alt="logo" />
     </Flex>
   );
 }
